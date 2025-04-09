a91e50fa7c82a165d043e831fb7f8d265fbf81e7
Amanda Wahyu <133777917+amandawahyu@users.noreply.github.com>
2025-04-10 01:27:37 +0700

    Add files via upload

---
+++ practice advance conditional/3.js
@@ -0,0 +1,31 @@
+// ============== 3 ===============
+/*
+Buatlah kode javascript menggunakan pseudocode di bawah ini
+
+STORE angka1 WITH ANY POSITIVE NUMBER
+STORE angka2 WITH ANY POSITIVE NUMBER
+STORE operator WITH ANY STRING
+STORE total WITH -1
+
+SWITCH operator
+    CASE '+'
+        SET total WITH angka1 PLUS angka2
+    CASE '-'
+        SET total WITH angka1 MINUS angka2
+    CASE '*'
+        SET total WITH angka1 TIMES angka2
+    CASE '/'
+        SET total WITH angka1 DEVIDE angka2
+    CASE '%'
+        SET total WITH angka1 MODULUS angka2
+    DEFAULT
+        DISPLAY "Invalid Operator"
+END SWITCH
+DISPLAY total
+
+*/
+
+let angka1 = 10;
+let angka2 = 8;
+let operator = "uang";
+let total = -1;
